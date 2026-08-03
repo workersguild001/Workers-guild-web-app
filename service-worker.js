c462a847acd1b658ee064c757c8d3f7b6c662a18
workersguild001 <workersguild001@gmail.com>
2026-08-03 00:08:41 -0400

    Refactor service worker to use constants for caching

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,26 @@
+const CACHE_NAME = 'guild-v1';
+const ASSETS = [
+  '/',
+  'index.html',
+  'gateway.html',
+  'dashboard.html',
+  'profile.html',
+  'recruitment.html',
+  'payouts.html',
+  'success.html',
+  'owner.html',
+  'admin.html',
+  'manifest.json'
+];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('guild-v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        'index.html',
-        'gateway.html',
-        'recruitment.html',
-        'profile.html',
-        'payouts.html',
-        'manifest.json'
-      ]);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
